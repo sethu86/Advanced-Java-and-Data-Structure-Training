71966541c5f206bce077220240b1a10871a807b8
sethu86 <137753567+sethu86@users.noreply.github.com>
2023-07-11 09:15:59 +0530

    Create arithmetic operation.java

---
+++ arithmetic operation.java
@@ -0,0 +1,10 @@
+class test {
+  public static void main(String[] args) {
+    int a=5;
+    int b=20;
+    int c=a+b;
+    int d=b-a;
+    System.out.println(c);
+    System.out.println(d);
+  }
+}
